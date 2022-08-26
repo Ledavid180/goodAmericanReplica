785272c11b55f698c039f5fbe18110dfbf8dda9d
Lauren David <ledavid180@gmail.com>
2022-08-26 14:04:42 -0500

    Adding JS

---
+++ js/app.js
@@ -0,0 +1,14 @@
+class ImagePicker {
+    constructor() {
+        this.imageObjects = {
+            item1: {
+                id: 1,
+                name: 'demin tiny ties bottom',
+                img: './media/denimBralletLight1.png',
+                img2: './media/denimBRalletLight2.png',
+                price: '$70',
+                
+            }
+        }
+    }
+}
